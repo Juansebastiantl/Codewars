5ad0153690596cd41af852dc00922776ea4d42d1
juansebastian.torres.laguado <juansebastian.torres.laguado@bbva.com>
2023-09-04 19:58:50 -0500

    Codewars

---
+++ src/codewars/codeWars.java
@@ -0,0 +1,81 @@
+package codewars;
+
+public class codeWars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Binary binary = new Binary();
+		System.out.println(binary.getBinary(5, 9));
+		
+
+
+
+		}
+
+		
+
+	  		   
+	}
+
+	
+
+
+
+class Binary {
+	
+	public String getBinary(int a, int b)
+	{
+		int entero = a+b;
+		String binario = Integer.toBinaryString(entero);
+		return binario;
+	}
+
+	
+}
+
+class Vowels {
+
+	  public static int getCount(String str) {
+	    
+		  char[ ] letras = new char[str.length()];
+		  int count = 0;
+		  for(int i=0; i < str.length(); i++ )
+		  {
+			  letras[i] = str.charAt(i);
+			  if (letras[i] == 'a')
+			  {
+				  count++;
+			  }
+			  
+			  else if (letras[i] == 'e')
+			  {
+				  count++;
+			  }
+			  else if (letras[i] == 'i')
+			  {
+				  count++;
+			  }
+			  else if (letras[i] == 'o')
+			  {
+				  count++;
+			  }
+			  else if (letras[i] == 'u')
+			  {
+				  count++;
+			  }
+			  
+		  }
+		  return count;
+	    
+	  }
+	}
+
+class Vowels2 {
+
+    public static int getCount(String str) {
+        return str.replaceAll("(?i)[^aeiou]", "").length();
+    }
+
+}
+
+
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module executionService {
+}
